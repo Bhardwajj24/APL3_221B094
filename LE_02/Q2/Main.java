40c80275207985379ef341d919fb9285139a7d75
221B094 <221B094@juet.co.in>
2025-01-31 10:18:18 +0530

    done with lab 2

--- LE_02/Q2/Main.java
+++ LE_02/Q2/Main.java
@@ -3,6 +3,5 @@ public class Main
 	public static void main(String[] args) {
 		Voice voice=new Voice();
 		voice.prepareVoice();
-		voice.hear();
 	}
 }
--- LE_02/Q2/Voice.java
+++ LE_02/Q2/Voice.java
@@ -1,17 +1,17 @@
 class Voice
 {
-    Animal [] animal=new Animal[5];
     void prepareVoice()
     {
+        Animal [] animal=new Animal[5];
         animal[0]=new Lion();
         animal[1]=new Goat();
         animal[2]=new Cow();
         animal[3]=new Tiger();
         animal[4]=new Pig();
-        
+        hear(animal);
     }
     
-    void hear()
+    void hear(Animal[] animal)
     {
         for(int i=0;i<5;i++)
         {
